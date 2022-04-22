f5d720826e3ff1553a36a3aaa30c20fa3c90a320
technoshailesh <cs19118@glbitm.ac.in>
2022-04-23 03:14:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MovieList from "./component/MovieList";
+import MovieListHeading from "./component/MovieListHeading";
+import SearchBox from "./component/SearchBox";
 
 function App() {
+  const [movies, SetMovies] = useState([{
+    "Title": "Star Wars",
+    "Year": "1977",
+    "imdbID": "tt0076759",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode V - The Empire Strikes Back",
+    "Year": "1980",
+    "imdbID": "tt0080684",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode VI - Return of the Jedi",
+    "Year": "1983",
+    "imdbID": "tt0086190",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+},
+{
+    "Title": "Star Wars: Episode VII - The Force Awakens",
+    "Year": "2015",
+    "imdbID": "tt2488496",
+    "Type": "movie",
+    "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
+},{
+  "Title": "Star Wars",
+  "Year": "1977",
+  "imdbID": "tt0076759",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+},
+{
+  "Title": "Star Wars: Episode V - The Empire Strikes Back",
+  "Year": "1980",
+  "imdbID": "tt0080684",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+},
+{
+  "Title": "Star Wars: Episode VI - Return of the Jedi",
+  "Year": "1983",
+  "imdbID": "tt0086190",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BOWZlMjFiYzgtMTUzNC00Y2IzLTk1NTMtZmNhMTczNTk0ODk1XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg"
+},
+{
+  "Title": "Star Wars: Episode VII - The Force Awakens",
+  "Year": "2015",
+  "imdbID": "tt2488496",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SX300.jpg"
+},]);
+  const [searchValue, setSearchValue] = useState([]);
+
+  const getMoviesRequest = async () => {
+    const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=616d0216`;
+    const response = await fetch(url);
+    const responseJson = await response.json();
+    if(responseJson.Search){
+
+  
+    SetMovies(responseJson.Search);  }
+  };
+
+  useEffect(() => {
+    getMoviesRequest();
+  }, [searchValue]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container-fluid movie-app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+        <MovieListHeading heading="Movies" />
+        <SearchBox searchValue={searchValue}setSearchValue={setSearchValue} />
+      </div>
+      <div className="row">
+        <MovieList movies={movies} />
+      </div>
     </div>
+   
+   
+     <div>
+
+       <h3>Developed by Shailesh Patel</h3>
+     </div>
+     
+     </>
+    
+
+  
   );
 }
 
